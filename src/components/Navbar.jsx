05a4fa015b6749a14c2c825881080696d7f58f1c
danielalagba97-tech <danielalagba97@gmail.com>
2026-05-22 16:36:41 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,21 +8,23 @@ function Navbar() {
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
-        padding: "15px 60px",
+        padding: "10px max(2vw, 20px)",
         background: "#000",
         borderBottom: "2px solid gold",
         position: "sticky",
         top: "0",
         zIndex: "1000",
+        flexWrap: "wrap",
+        gap: "10px"
       }}
     >
       {/* Logo + Brand Name */}
-      <div style={{ display: "flex", alignItems: "center", gap: "25px" }}>
+      <div style={{ display: "flex", alignItems: "center", gap: "max(1vw, 10px)", flexShrink: 1 }}>
         <img
           src={logo}
           alt="King Arthur Food Palace"
           style={{
-            height: "120px",
+            height: "clamp(45px, 8vw, 100px)",
             objectFit: "contain",
           }}
         />
@@ -32,8 +34,9 @@ function Navbar() {
             color: "gold",
             fontWeight: "900",
             margin: 0,
-            fontSize: "30px",
-            letterSpacing: "1px",
+            fontSize: "clamp(15px, 2.2vw, 28px)",
+            letterSpacing: "0.5px",
+            whiteSpace: "nowrap"
           }}
         >
           King Arthur Food Palace
@@ -41,7 +44,7 @@ function Navbar() {
       </div>
 
       {/* Navigation Links */}
-      <div>
+      <div style={{ display: "flex", alignItems: "center", gap: "max(1.5vw, 10px)", flexWrap: "wrap" }}>
         <Link to="/" style={linkStyle}>Home</Link>
         <Link to="/menu" style={linkStyle}>Menu</Link>
         <Link to="/about" style={linkStyle}>About</Link>
@@ -53,21 +56,20 @@ function Navbar() {
 }
 
 const linkStyle = {
-  marginRight: "25px",
   color: "white",
   textDecoration: "none",
   fontWeight: "600",
-  fontSize: "16px",
+  fontSize: "clamp(13px, 1.2vw, 16px)",
 };
 
 const orderStyle = {
   background: "gold",
-  padding: "10px 18px",
+  padding: "6px max(1.2vw, 12px)",
   borderRadius: "6px",
   color: "black",
   textDecoration: "none",
   fontWeight: "bold",
-  fontSize: "16px",
+  fontSize: "clamp(13px, 1.2vw, 16px)",
 };
 
 export default Navbar;
